cfa4c58bd34a81544789948af4d7a1af7275164c
DarkDev <darkdev_brand@riseup.net>
2024-05-02 16:00:28 +0300

    refactoring layout structure

--- src/components/widgets/sections/Carousel.svelte
+++ src/components/widgets/sections/Carousel.svelte
@@ -1,13 +1,13 @@
 <script>
-	export let progects;
+	export let projects;
 </script>
 
-<div class="progects-wrap w-full bg-info">
-	<div class="progects bg-info">
-		{#each progects as progect}
-			<div class="progect">
-				<a href={progect.link}>
-					<img src={progect.logo} height="100" width="auto" alt="" />
+<div class="projects-wrap w-full bg-info">
+	<div class="projects bg-info">
+		{#each projects as project}
+			<div class="project">
+				<a href={project.link}   target="_blank">
+					<img src={project.logo} height="100" width="auto" alt="" />
 				</a>
 			</div>
 		{/each}
@@ -32,7 +32,7 @@
 			transform: translateX(calc(-250px * 3));
 		}
 	}
-	.progects-wrap {
+	.projects-wrap {
 		/* background: white; */
 		box-shadow: 0 10px 20px -5px rgba(0, 0, 0, 0.125);
 		height: 200px;
@@ -41,8 +41,8 @@
 		position: relative;
 		/* width: 100%; */
 	}
-	.progects-wrap::before,
-	.progects-wrap::after {
+	.projects-wrap::before,
+	.projects-wrap::after {
 		/* background: linear-gradient(to right, white 0%, rgba(255, 255, 255, 0) 100%); */
 		content: '';
 		height: 200px;
@@ -50,22 +50,22 @@
 		width: 200px;
 		z-index: 2;
 	}
-	.progects-wrap::after {
+	.projects-wrap::after {
 		right: 0;
 		top: 0;
 		transform: rotateZ(180deg);
 	}
-	.progects-wrap::before {
+	.projects-wrap::before {
 		left: 0;
 		top: 0;
 	}
-	.progects-wrap .progects {
+	.projects-wrap .projects {
 		-webkit-animation: scroll 20s linear infinite;
 		animation: scroll 20s linear infinite;
 		display: flex;
 		width: calc(250px * 3);
 	}
-	.progect {
+	.project {
 		display: flex;
 		justify-items: center;
 		padding-top: 50px;
