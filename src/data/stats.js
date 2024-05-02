771e1387b3edebc30758d202d69efe199d17341b
DarkDev <darkdev_brand@riseup.net>
2024-05-02 15:59:05 +0300

    refactoring stats

---
+++ src/data/stats.js
@@ -0,0 +1,25 @@
+const statisticsData = [
+	{
+		title: 'Stworzone gier planszowych',
+		count: 16,
+		fade: 'up'
+	},
+	{
+		title: 'Zorganizowane warsztaty',
+		count: 40,
+		fade: 'down'
+	},
+	{
+		title: 'Zorganizowane lokalne inicjatywy',
+		count: 7,
+		fade: 'up'
+	},
+	{
+		title: 'Wsperanych NGO',
+		count: 20,
+		fade: 'down'
+	}
+];
+
+
+export default statisticsData;
