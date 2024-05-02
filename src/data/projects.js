4ab16aa31b872d7a8590e77a5197f4da9f155a07
DarkDev <darkdev_brand@riseup.net>
2024-05-02 15:58:28 +0300

    refactoring projects

---
+++ src/data/projects.js
@@ -0,0 +1,49 @@
+const projects = [
+	{
+		title: '44FabLab',
+		description: [
+			'Otwarta przestrzeń dla twórczości technicznej i programowania oraz tworzenia edukacyjnych gier planszowych.'
+		],
+		logo: '/images/logos/44fab_lab.svg',
+		link: 'https://44fablab.vercel.app/',
+		flip: 'right'
+	},
+	{
+		title: 'ITingo',
+		description: [
+			'ITingo to platforma internetowa, która łączy IT wolontariuszy, organizacje pozarządowe, biznes i startupy - w celu dostarczenia projektów IT online.'
+		],
+		logo: '/images/logos/itingo.svg',
+		link: 'https://itingo.vercel.app/',
+		flip: 'right'
+	},
+
+	{
+		title: 'Yellow elephant',
+		description: [
+			'Comiesięczny turniej szachów klasycznych zarówno dla amatorów jak i tych bardziej zaawansowanych graczy.',
+			'Turniej odbywa się każda środa w pierwszym tygodniu miesiąca.'
+		],
+		logo: '/images/logos/yellow_elephant.svg',
+		link: 'https://yellow-elephant.vercel.app/',
+		flip: 'left'
+	},
+	{
+		title: '44games',
+		description: ['Tworzenie, rozwój i produkcja prototypów edukacyjnych gier planszowych.'],
+		logo: '/images/logos/44_cubes.svg',
+		link: 'https://44games.vercel.app/',
+		flip: 'left'
+	},
+	{
+		title: 'AgileKIT Community',
+		description: [
+			'Społeczność AgileKIT zaprasza praktyków wszystkich zawodów do wspólnej edukacji, rozrywki i rozwoju - zarówno poprzez spotkania wirtualne, jak i bezpośrednie.'
+		],
+		logo: '/images/logos/agileKit.svg',
+		link: 'https://agile-fans.vercel.app/',
+		flip: 'right'
+	}
+];
+
+export default projects;
