0edf6a4fa868a305bd5867b427b7c1c842040547
DarkDev <darkdev_brand@riseup.net>
2024-05-06 20:20:33 +0300

    add show / hide logic

--- src/components/widgets/sections/Faq.svelte
+++ src/components/widgets/sections/Faq.svelte
@@ -2,12 +2,6 @@
 	export let faqList;
 </script>
 
-<!-- 		data-aos="flip-down"
-		data-aos-delay="100"
-		data-aos-duration="1400"
-		data-aos-once="false"
-		data-aos-mirror="true"
- -->
 {#each faqList as faq}
 	<div
 		class=" max-w-4xl w-full bg-info rounded-lg my-2 "
@@ -22,8 +16,7 @@
 				class="flex  rounded-lg w-full items-center justify-between border border-b-0 border-gray-200 bg-info p-5 text-left  font-medium text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 "
 			>
 				<span>{faq.title} <i class="icon-question color-viol" /> </span>
-				<span><i class="{!faq.showText ? 'icon-arrow-down' : 'icon-arrow-up'} color-viol" /></span>
-				<!-- <svg data-accordion-icon="" class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg> -->
+				<svg data-accordion-icon="" class="w-6 h-6 {!faq.showText ? '' : 'rotate-180'}  shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 			</button>
 		</p>
 		{#if faq.showText}
