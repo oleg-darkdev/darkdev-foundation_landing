db29b855bf852d0297629b4022bb6a1ab01d0ba7
DarkDev <darkdev_brand@riseup.net>
2024-05-06 20:19:32 +0300

    upd faq

--- src/data/landingFaqList.js
+++ src/data/landingFaqList.js
@@ -1,74 +1,41 @@
 const faqList = [
 	{
-		title: 'Lorem ipsum dolor sit amet',
+		title: 'W jakim regionie organizujecie warsztaty?',
 		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			'Gdańsk, Sopot, Gdynia, Warszawa.',
+			'Istnieje możliwość prowadzenia niektórych warsztatów - online.'
 		]
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet',
+		title: 'Ile kosztuje organizacja szkolenia dla NGO?',
 		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+			'Jeśli organizacja pozarządowa chce zorganizować zajęcia w ramach projektu społecznego z dotacji - koszt zajęcia wynosi od 200 do 350zł brutto.',
 		]
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet',
-		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		]
+		title: 'Ile kosztuje organizacja szkolenia dla firmy?',
+		desctiption: [			
+      'Jeśli jesteś przedstawicielem biznesu i chcesz zorganizować zajęcia w swojej firmie - koszt to 200zł brutto, za godzinę pracy.',
+]
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet',
+		title: 'Ile kosztuje organizacja szkolenia dla nieformalną grupę aktywistów ?',
 		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		]
+      'Jeśli jesteście nieformalną grupą aktywistów, herbata i ciastka są na waszą kieszeń, a my poprowadzimy dla was darmową lekcję.'
+]
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet',
-		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		]
+		title: 'W jaki sposób organizowane są warsztaty?',
+		desctiption: ['Czas trwania warsztatów wynosi od 120 minut do 480 minut - w zależności od wybranego tematu.', 'Dla podniesienia jakości wykorzystujemy grywalizację w postaci edukacyjnych gier planszowych pochodzących z projektu 44Games.']
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet',
-		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		]
+		title: 'W jakich językach organizowane są zajęcia?',
+		desctiption: ['Polski, Angielski, białoruski, Ukraiński, Rosyjski.']
 	},
 	{
-		title: 'Lorem ipsum dolor sit amet',
-		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		]
+		title: 'Czy istnieje opcja kupna edukacyjnych gier planszowych, z których korzystacie?',
+		desctiption: ['Tak, skontaktuj się z nami, a prześlemy Ci kontakty do producenta 44Games.']
 	},
-	{
-		title: 'Lorem ipsum dolor sit amet',
-		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		]
-	},
-	{
-		title: 'Lorem ipsum dolor sit amet',
-		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		]
-	},
-	{
-		title: 'Lorem ipsum dolor sit amet',
-		desctiption: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-			'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-		]
-	}
 ];
 
 export default faqList;
