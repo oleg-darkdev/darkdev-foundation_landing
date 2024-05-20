bc8e210ab6668e22e4ee2a6d0dbb06fc67119694
DarkDev <darkdev_brand@riseup.net>
2024-05-20 23:45:09 +0300

    upd content

--- src/data/noOrgsWorkshops.js
+++ src/data/noOrgsWorkshops.js
@@ -1,44 +1,44 @@
 const noOrgsWorkshops = [
 	{
 		id: 0,
-		img: 'boardgame-mechanics.svg',
-		title: 'Prototypowanie gier planszowych',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
-		link: '/'
-	},
-	{
-		id: 0,
-		img: 'boardgame-design.svg',
-		title: 'Projektowanie gier planszowych',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
-		link: '/'
-	},
-	{
-		id: 0,
-		img: 'boardgame-prototype.svg',
-		title: 'Testowanie prototypów gier planszowych',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
-		link: '/'
-	},
-	{
-		id: 0,
 		img: 'tech-site.svg',
 		title: 'Tworzenie stron internetowych',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		desc: 'Programowanie stron internetowych przy użyciu języków programowania HTML, CSS i JavaScript.',
 		link: '/'
 	},
 	{
 		id: 0,
 		img: 'tech-3d.svg',
 		title: 'Drukarki 3D',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		desc: 'Poznanie technologii druku 3D oraz sposoby naprawy i obsługi drukarek 3D.',
 		link: '/'
 	},
 	{
 		id: 0,
 		img: 'tech-cnc.svg',
 		title: 'Urządzenia CNC',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		desc: 'Praca z urządzeniami CNC, ich naprawa i konserwacja.',
+		link: '/'
+	},
+	{
+		id: 0,
+		img: 'boardgame-mechanics.svg',
+		title: 'Prototypowanie gier planszowych',
+		desc: 'Własnoręczne tworzenie gier planszowych przy użyciu wielu narzędzi i materiałów.',
+		link: '/'
+	},
+	{
+		id: 0,
+		img: 'boardgame-design.svg',
+		title: 'Projektowanie gier planszowych',
+		desc: 'Projektowanie i tworzenie gier planszowych przy użyciu nowoczesnych technologii i podejść.',
+		link: '/'
+	},
+	{
+		id: 0,
+		img: 'boardgame-prototype.svg',
+		title: 'Testowanie prototypów gier planszowych',
+		desc: 'Testowanie gier planszowych z grupami fokusowymi i społecznością gamingową w celu uzyskania opinii.',
 		link: '/'
 	}
 ];
--- src/data/orgWorkshopsList.js
+++ src/data/orgWorkshopsList.js
@@ -3,49 +3,49 @@ const orgWorkshopsList = [
 		id: 0,
 		img: 'it-del.svg',
 		title: 'Sposoby ustalania priorytetów zadań',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		desc: 'Praktyczne zastosowanie narzędzia do dobrowolnego delegowania zadań w zespole.',
 		link: '/'
 	},
 	{
 		id: 0,
 		img: 'it-del.svg',
 		title: 'Delegowanie zadań w zespole',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		desc: 'Zastosowanie w praktyce narzędzia do ustalania priorytetów zadań w zespole bez użycia przemocy.',
 		link: '/'
 	},
 	{
 		id: 0,
 		img: 'it-del.svg',
 		title: 'Metody estymacji i planowanie zadań',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		desc: 'Skolenie z wykorzystaniem najprostszych technik i oprogramowania do szacowania i planowania zadań.',
 		link: '/'
 	},
 	{
 		id: 0,
 		img: 'it-del.svg',
 		title: 'Narzędzie do rekrutacji i HR',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		desc: 'Skolenie z wykorzystaniem narzędzia do procesu rekrutacji w oparciu o istniejące cele w zespole i kandydata.',
 		link: '/'
 	},
 	{
 		id: 0,
 		img: 'it-del.svg',
 		title: 'Metoda Kanban do organizacji pracy',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		desc: 'Skolenie na temat wdrażania metody Kanban w celu efektywnej organizacji i wizualizacji pracy.',
 		link: '/'
 	},
 	{
 		id: 0,
 		img: 'it-del.svg',
-		title: ' Scrum dla samoorganizujących się zespołów',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		title: 'Scrum dla zespołów.',
+		desc: 'Skolenie na wdrożenie metodologii Scrum w celu poprawy efektywności i przewidywalności pracy zespołowej.',
 		link: '/'
 	},
 	{
 		id: 0,
 		img: 'it-del.svg',
 		title: 'Narzędzia do facylitacji i komunikacji w zespole',
-		desc: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries.',
+		desc: 'Praktyczna sesja z wykorzystaniem narzędzi służących ułatwieniu i poprawie komunikacji w zespole.',
 		link: '/'
 	},
 	{
